114256ec39eea8fe239d843fef874a8600ef4ad3
Arthur Gomes <arthur.g.barros8@aluno.senai.br>
2023-09-29 11:45:21 -0300

    Projeto Pensador CONFIG:PRONTO, MODELS:CRIANDO

---
+++ routes/toughtsRouters.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const ToughtController = require('./controllers/ToughtController');
+
+router.get('/', ToughtController.showTought);
+module.exports = router
