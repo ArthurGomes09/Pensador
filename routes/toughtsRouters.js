59e4ba0195eef565218b099ff56639bc2710b0ac
Arthur Gomes <109075801+ArthurGomes09@users.noreply.github.com>
2023-10-11 09:54:52 -0300

    Delete routes directory

--- routes/toughtsRouters.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ToughtController = require('./controllers/ToughtController');
-
-router.get('/', ToughtController.showTought);
-module.exports = router
