9d1c154efe9a3de29e3a9100350e076c4dcd9b3b
Arthur Gomes <109075801+ArthurGomes09@users.noreply.github.com>
2023-10-20 09:44:00 -0300

    Add files via upload

---
+++ pensador-main-finalizado/controllers/ToughtController.js
@@ -0,0 +1,86 @@
+const { request } = require('express');
+const { where } = require('sequelize');
+const Tought = require('../models/Tought')
+const User = require('../models/User')
+//importar o middleware de autenticação do user
+
+module.exports = class ToughtController{
+  static async showToughts(request, response){
+    const toughtsData = await Tought.findAll({
+      include: User,
+    })
+    const toughts = toughtsData.map((result)=>result.get({plain:true}))
+    console.log(toughts)
+    return response.render('toughts/home', {toughts})//Mostrando um view
+  };
+  static async dashboard(req,res){
+    const user = await User.findOne({
+      where:{
+        id: req.session.userId
+      },
+      include:Tought,
+      plain:true
+    })
+    
+    if(user.Toughts.lenght == 0){
+      req.flash('message', 'Você ainda não publicou nenhum pensamento!')
+      return res.render('/login')
+    }
+    const toughts = user.Toughts.map((result) => result.dataValues);
+    return res.render('toughts/dashboard', {toughts:toughts})
+  };
+  static createTought(req,res){
+    return res.render('toughts/create')
+  };
+  static async createToughtSave(req,res){
+   const tought = {
+    title: req.body.title,
+    UserId: req.session.userId
+   } 
+   try {
+    await Tought.create(tought)
+    req.flash('message', 'Pensamento criado com sucesso!')
+    req.session.save(()=>{
+      res.redirect('/toughts/dashboard')
+    })
+   } catch (error) {
+    console.log(`Aconteceu um erro: ${error}`)
+   }
+  };
+  static async removeToughtSave(req,res){
+    const {id} = req.body
+    console.log(id)
+    const userId = req.session.userId
+    try {
+      await Tought.destroy({where:{id:id, UserId:userId}})
+      req.flash("message", "Pensamento excluído com sucesso!")
+
+      req.session.save(()=>{
+        res.redirect('/toughts/dashboard')
+      })
+    } catch (error) {
+      console.log(`Aconteceu um erro: ${error}`)
+    }
+  };
+  static async editTought(request, response) {
+    try {
+      const tought = await Tought.findOne({ where: { id: request.params.id }, raw: true })
+
+      console.log(tought);
+
+      return response.render('toughts/edit', { tought: tought })
+    } catch (error) {
+      console.error('Não foi possível encontrar o pensamento, erro' + error)
+    }
+  }
+  static async editToughtSave(request, response) {
+    try {
+      await Tought.update({ title: request.body.title }, { where: { id: request.params.id } })
+      request.session.save(()=>{
+        return response.redirect('/toughts/dashboard')
+      })
+    } catch (error) {
+      console.error('Não foi possível editar o pensamento, erro' + error)
+    }
+  }
+}
